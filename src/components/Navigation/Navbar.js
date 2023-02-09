74c5bc498b3d3ed7f16fa667b1aee1f4334538b4
MK <mortezakarim0@gmail.com>
2023-02-09 14:27:14 +0000

    store is now holding iso2 and country Name

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,41 +1,66 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 
-import { Nav, DropdownMenu, Select } from "./Navbar.elements";
+import { DropdownMenu } from "./Navbar.elements";
 
 import countryNames from "../../resources/countryBorders.json";
+import { useDispatch } from "react-redux";
+import {
+  addCurrentCountry,
+  addCurrentIso,
+} from "../features/CountryInfo/currentCountrySlice";
+
 const Navbar = () => {
+  const dispatch = useDispatch();
   const [listOfCountries, setListOfCountries] = useState([]);
+  const [currentCountry, setCurrrentCountry] = useState({});
 
   const features = countryNames.features;
   useEffect(() => {
     function getAndSortCountries() {
-      const countryArr = features.map((element) => element.properties)
-      countryArr.sort((a, b) => a.name.localeCompare(b.name))
+      // initialization
+      const countryArr = features.map((element) => element.properties);
+      //https://www.freecodecamp.org/news/destructure-object-properties-using-array-map-in-react/
+      countryArr.sort((a, b) => a.name.localeCompare(b.name)); // sorting array based on countryname 
       setListOfCountries(countryArr);
     }
-    getAndSortCountries()
+    getAndSortCountries(); // decalartion
   }, [features]);
 
+  // function that handles when dropdown menu is changed
+  function updateMyStore(event) {
+    event.preventDefault();
 
-  // OnClick 
+    let name; // this is array ["Afghanistan", "Af"]
+    if (event.target.value && event.target.value !== null) {
+      name = event.target.value.split(",");
+    }
+    dispatch(addCurrentCountry(name[0]));
+    dispatch(addCurrentIso(name[1]));
+
+    // make Api Request for country info 
+
+
+    // save the value return from that API call into redux 
+
+
+
+  }
 
   return (
-    <DropdownMenu>
-      <option disabled >
-        Select a Country...
-      </option>
+    <DropdownMenu onChange={(event) => updateMyStore(event)}>
+      <option disabled>Select a Country...</option>
 
-      {
-        listOfCountries &&
+      {listOfCountries &&
         listOfCountries.map((element, index) => (
-          <option key={index} value={element.name}>
+          <option key={index} value={`${element.name},${element.iso_a2}`}>
             {element.name} ({element.iso_a2})
           </option>
-        ))
-      }
-    </DropdownMenu >
+        ))}
+    </DropdownMenu>
   );
 };
 
 export default Navbar;
+// the dropdown menu takes in name/iso-a2 properties from json data array
+//
--- src/components/features/CountryInfo/currentCountrySlice.js
+++ src/components/features/CountryInfo/currentCountrySlice.js
@@ -2,23 +2,25 @@ import { createSlice } from "@reduxjs/toolkit";
 
 
 const currentCountrySlice = createSlice({
-    name: 'afghanistan',
-    countryInfo: {},
-    initialState:{
-        name: [],
-        countryInfo: {
-        
-        }
+    name: 'countryInfo',
+    initialState: {
+        name: "",
+        iso_a2: '',
+        countryInfo: {}
     },
-    reducers:{
-        selectName(state, action) {
-            state.name=action.payload;
+    reducers: {
+        addCurrentCountry(state, action) {
+            state.name = action.payload;
         },
         updateCountryInfo(state, action) {
-            state.countryInfo= action.payload;
+            state.countryInfo = action.payload;
+        },
+        addCurrentIso(state, action) {
+            state.iso_a2 = action.payload;
         }
+
     }
 
 })
-export const {selectName, updateCountryInfo} = currentCountrySlice.actions;
+export const { addCurrentCountry, updateCountryInfo, addCurrentIso } = currentCountrySlice.actions;
 export default currentCountrySlice.reducer;
